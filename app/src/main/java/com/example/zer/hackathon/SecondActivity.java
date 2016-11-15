693d4b2a5cfeb433713f555d7ec98a29699d76d8
DSemk <samik6677@gmail.com>
2016-11-15 22:10:04 +0300

    add final logic activity

--- app/src/main/java/com/example/zer/hackathon/SecondActivity.java
+++ app/src/main/java/com/example/zer/hackathon/SecondActivity.java
@@ -1,31 +1,44 @@
 package com.example.zer.hackathon;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
 import java.util.HashMap;
 
 /**
  * Created by samik on 15.11.2016.
  */
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity  {
     TextView sun;
     TextView moon;
     TextView wind;
     TextView rain;
+
+    TextView weekDay;
+    TextView today;
+    TextView date;
+    ImageView icon;
     Model model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
         initialize();
-
+        Intent intent = getIntent();
+        int modelNum = intent.getIntExtra("index",0);
+        setDetailsInfo(modelNum);
 
     }
+
 
 
     public void setDetailsInfo(int modelNum) {
@@ -40,6 +53,16 @@ public class SecondActivity extends AppCompatActivity {
 
         setTextDetail(sunVolum,moonVolum,windVolume,rainVolume);
 
+        String today = "";
+        if (modelNum == 0){
+            today = "сегодня";
+        }
+        String dateVolume = String.valueOf(Calendar.getInstance());
+        String url = getIconUrl();
+        String weekDay = weekDay();
+
+        setSimpleDatail(weekDay,today,dateVolume,url);
+
     }
 
     private void setTextDetail(String sunVolume, String moonVolume, String windVolume, String rainVolume) {
@@ -47,12 +70,40 @@ public class SecondActivity extends AppCompatActivity {
         moon.setText(moonVolume);
         rain.setText(rainVolume);
         wind.setText(windVolume);
+
+    }
+
+    private void setSimpleDatail(String weekDayVolume, String todayVolume, String dateVolume, String url){
+        weekDay.setText(weekDayVolume);
+        today.setText(todayVolume);
+        date.setText(dateVolume);
+        Picasso.with(this).
+                load(url).
+                into(icon);
     }
 
     private void initialize() {
-        rain = (TextView) findViewById(R.id.da_rainAndAll);
-        sun = (TextView) findViewById(R.id.da_sunUpDown);
-        wind = (TextView) findViewById(R.id.da_wind);
-        moon = (TextView) findViewById(R.id.da_moonUpDown);
+        rain = (TextView) findViewById(R.id.chanceOfRain);
+        sun = (TextView) findViewById(R.id.sunUpDown);
+        wind = (TextView) findViewById(R.id.windSpeed);
+        moon = (TextView) findViewById(R.id.moonUpDown);
+
+        weekDay = (TextView)findViewById(R.id.tvNowDay);
+        today = (TextView)findViewById(R.id.tvNow); // показывает если выбран сегоднешний день
+        icon = (ImageView)findViewById(R.id.ivNow);
+        date = (TextView)findViewById(R.id.tvDataNow);
+
+    }
+
+
+    public String getIconUrl() {
+        //TODO
+        return null;
+    }
+
+
+    public String weekDay() {
+        //TODO
+        return null;
     }
 }
